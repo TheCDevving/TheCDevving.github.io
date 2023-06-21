506c541a52cd26203897266485e74aa39704078e
TheCDevving <thecdevving@gmail.com>
2023-06-20 23:22:06 -0500

    Update website files

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
 window.addEventListener("scroll", function () {
-    var scrollButton = document.querySelector(".scroll-to-top");
-    if (window.pageYOffset > 100) {
-      scrollButton.style.display = "block";
-    } else {
-      scrollButton.style.display = "none";
-    }
-  });
-  
-  document.querySelector(".scroll-to-top").addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-  
-  // Chat window functionality
-  
-  var chatMessages = document.getElementById('chat-messages');
-  var chatInput = document.getElementById('chat-input');
-  
-  function sendMessage() {
-    var message = chatInput.value;
-  
-    if (message.trim() !== '') {
-      var newMessage = document.createElement('div');
-      newMessage.innerText = message;
-      chatMessages.appendChild(newMessage);
-      chatInput.value = '';
-      chatMessages.scrollTop = chatMessages.scrollHeight;
-    }
+  var scrollButton = document.querySelector(".scroll-to-top");
+  if (window.pageYOffset > 100) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
   }
-  
-  chatInput.addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-      sendMessage();
-    }
-  });
-  
+});
+
+document.querySelector(".scroll-to-top").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
